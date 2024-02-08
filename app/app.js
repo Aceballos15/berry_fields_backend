5b961a48913945d108ccafa4abb9000b1d51e82f
Aceballos <ceballoscardonaalexander@gmail.com>
2024-02-08 17:21:33 -0500

    try log to checksum 2.1

--- app/app.js
+++ app/app.js
@@ -57,7 +57,6 @@ app.post('/api/Signature', (req, res)=>{
             }
         ]
         
-        console.log(data) 
     
         DATA.push(data) 
         res.status(200).send(data)
@@ -91,7 +90,7 @@ app.post('/api/res/nidum', async(req, res)=>{
     //llamado a la funcion de validacion de cheksum 
     const checksum = await assignChecksum(response)
     
-    console.log(`Checksum nidum: ${console.log(response)}`)
+    console.log(`Checksum nidum: ${console.log(response.signature)}`)
 
     console.log(`Checksum Moncho: ${checksum}`)
 
